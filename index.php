c50bcda0217f9387473ad6cac94bbe791d666dea
balram khichar <Balram@balrams-MacBook-Pro.local>
2015-11-03 10:19:20 +0530

    initialized project

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>QuoteDay</title>
+        <style>
+         body {
+            text-align:center;
+            font-family: 'Roboto' sans-serif;
+         }         
+        </style>
+</head>
+<body>
+<h2>Coming Soon</h2>
+</br></br></br></br>
+<a href="http://www.iambalram.com">Contact</a>
+</body>
+</html>		
